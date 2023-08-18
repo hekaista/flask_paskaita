cc7a3d1a06806231a976fcfcef9817fcb6834551
Heka <code@epixel.lt>
2023-08-18 15:29:46 +0300

    route "orai"

--- flaskapp_01.py
+++ flaskapp_01.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 def home():
     return "<h1>Čia yra mano pirmasis namų puslapis su Flask</h1>"
 
-@app.route("/<kintamasis>") #apsišašom adresą, ir pasakom kad po slasho bus išvestas kažkas ko neturim aprašę linkuose
+@app.route("/orai")
+def orai():
+    return "<h1>Labai karšta</h1>"
+
+@app.route("/<kintamasis>")
 def user(kintamasis):
     return f"<h1>Sveiki {kintamasis} atvykę į Flask puslapį</h1>"
+
 
 if __name__ == "__main__":
     app.run()
