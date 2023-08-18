a4ebb1e75951ef6d32addc313e7bfdb8d4eff4fb
Heka <code@epixel.lt>
2023-08-18 16:35:36 +0300

    templeitai, forma

---
+++ flaskapp_02.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/orai")
+def orai():
+    return render_template("orai.html")
+
+
+@app.route("/ciklas")
+def ciklas():
+    return render_template("ciklas.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("forma.html")
+
+
+@app.route("/<kintamasis>")
+def user(kintamasis):
+    return render_template("vardas.html", sablono_kint=kintamasis)
+
+
+if __name__ == "__main__":
+    app.run()
