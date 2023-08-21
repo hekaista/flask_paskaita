e7a86846edd9dafc09a3eb46b480d01e5d54594f
Heka <code@epixel.lt>
2023-08-21 10:33:05 +0300

    pasisveikinimo forma jo ciklas

--- flaskapp_02.py
+++ flaskapp_02.py
@@ -23,6 +23,15 @@ def naujienos():
     return render_template("naujienos.html")
 
 
+@app.route("/pasisveikink5", methods=["GET", "POST"])
+def pasisveikink5():
+    if request.method == "GET":
+        return render_template("pasisveikink5_forma.html")
+    if request.method == "POST":
+        tekstas = request.form["laukelis"]
+        return render_template("pasisveikink5_rezultatai.html", tekstas=tekstas)
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
